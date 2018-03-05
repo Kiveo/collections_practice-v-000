6688dbf26274f4aea1d7ec57d2a83ae242d098ec
Maverick Tesini <mavericktesini@gmail.com>
2018-03-05 19:38:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,8 +43,6 @@ def find_a(array) #words that begin with "a" in a word array
   array.each {|word|
     if word.start_with?("a")
       new_array << word
-    # else
-    #   false
     end
   }
   new_array
